c5551e04b04023da3608d73795b26b174a78ed69
saiteja <saitejachicky@gmail.com>
2025-02-20 16:53:55 +0530

    intial-setup

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-     <h1 className="text-3xl font-bold underline text-green-400">
+     <h1 className="text-3xl font-bold underline text-green-300">
       Hello world!
     </h1>
     </>
