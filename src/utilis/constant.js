ec9da56f3a1ce6e38c1b89e095252c9d3fc15869
saiteja <saitejachicky@gmail.com>
2025-05-09 11:01:01 +0530

    new-push

--- src/utilis/constant.js
+++ src/utilis/constant.js
@@ -1,4 +1,4 @@
 // export const BASE_URL="/api"
 
-export const BASE_URL="http://localhost:5000"
-// export const BASE_URL="https://tinder-backend-bice.vercel.app"
+// export const BASE_URL="http://localhost:5000"
+export const BASE_URL="https://tinder-backend-bice.vercel.app"
